ded7e7ba23f144acd0199eb612a9ff0c9b99606b
Sukhbinder Singh <1002094@QuEST-RR01.COM>
2021-01-09 11:06:35 +0530

    Added setup

--- setup.py
+++ setup.py
@@ -7,5 +7,22 @@ setup(
     license="Private",
     description="Intersection Of two curves",
     author="sukhbinder",
-    author_email="sukh2010@yahoo.com"
+    author_email="sukh2010@yahoo.com",
+    license='MIT',
+    url = 'https://github.com/sukhbinder/intersection',
+    keywords = ['Intersection" "curves" numpy"],
+    
+    classifiers=[
+    'Development Status :: 4- Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3', 
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 2.7',
+  ], 
 )
