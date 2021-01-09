21d4021d2243e8f8f35d9a84d02a411b77663b9c
Sukhbinder Singh <1002094@QuEST-RR01.COM>
2021-01-09 11:28:58 +0530

    Dev statas

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     keywords = ["Intersection", "curves", "numpy"],
     
     classifiers=[
-    'Development Status :: 4- Beta',
+    'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
