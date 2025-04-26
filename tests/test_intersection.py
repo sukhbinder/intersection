bea64c52f57175ef10f07350ee608641f535e9a9
Sukhbinder Singh <sukhbindersingh@gmail.com>
2025-04-26 21:41:52 +0530

    Update test

--- tests/test_intersection.py
+++ tests/test_intersection.py
@@ -12,10 +12,13 @@ def test_basic():
 
     x2 = phi
     y2 = np.sin(phi)+2
-    x, y = intersection(x1, y1, x2, y2)
+    x, y, i,j = intersection(x1, y1, x2, y2)
 
     assert pytest.approx(x) == np.array([6.10765984, 8.36483107])
     assert pytest.approx(y) == np.array([1.82539714, 2.87208714])
+    assert pytest.approx(i) == np.array([18.19901912, 85.79448352])
+    assert pytest.approx(j) == np.array([43.95118922, 75.87403944])
+    
 
 
 def test_bug_overlapping_lines():
@@ -26,7 +29,7 @@ def test_bug_overlapping_lines():
     y1 = [100.,  25.,  25.,  25.,  20.,  20.,  20.,   0.]
     x2 = [0., 0., 2., 2., 2., 4., 4., 4.]
     y2 = [0.,  10.,  10.,  10.,  20.,   20.,  20., 100.]
-    x, y = intersection(x1, y1, x2, y2)
+    x, y ,i, j= intersection(x1, y1, x2, y2)
 
     assert pytest.approx(x) == np.array([2., 2., 2.])
     assert pytest.approx(y) == np.array([20.,  10., 20.])
