8822014547e12d32a7a1b9e100804fee0b2909f4
Sukhbinder Singh <sukhbindersingh@gmail.com>
2025-04-26 22:53:38 +0530

    Revert to 3.9

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
